844cafac59df4addc2a89afa6db0dcbf841fbd92
eyob05 <65434701+eyob05@users.noreply.github.com>
2021-01-12 22:18:19 -0600

    lab-02 edited

--- Lab-02/Q2/api/controllers/addition.controllers.js
+++ Lab-02/Q2/api/controllers/addition.controllers.js
@@ -1,6 +1,6 @@
 module.exports.addTwoNumbers=function(req,res){
     var urlNumber=parseInt(req.params.num1);
-    var queryStringNumber=parseInt(req.query.num2);
+    var queryStringNumber=parseInt(req.query.n2);
     var sum=urlNumber + queryStringNumber;
     
     var tot="sum of "+urlNumber+" + "+queryStringNumber+" = "+sum;
--- Lab-02/Q2/api/routes/index.js
+++ Lab-02/Q2/api/routes/index.js
@@ -2,5 +2,5 @@ var express= require("express");
 var router= express.Router();
 var additionControllers =require("../controllers/addition.controllers.js");
 
-router.route("/add/:num1/").get(additionControllers.addTwoNumbers);
+router.route("/sum/:n1/").get(additionControllers.addTwoNumbers);
 module.exports=router;
