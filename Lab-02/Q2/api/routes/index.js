426581c4f1abc4b1b0d2f3085afa26adbc553e4f
eyob05 <65434701+eyob05@users.noreply.github.com>
2021-01-14 15:01:00 -0600

    lab-03

--- Lab-02/Q2/api/routes/index.js
+++
@@ -1,6 +0,0 @@
-var express= require("express");
-var router= express.Router();
-var additionControllers =require("../controllers/addition.controllers.js");
-
-router.route("/sum/:n1/").get(additionControllers.addTwoNumbers);
-module.exports=router;
