d1d6d3521c758a161c50aaa16463dd10f6414441
eyob05 <65434701+eyob05@users.noreply.github.com>
2021-01-14 23:36:55 -0600

    lab-04

--- Lab-04/question-2/api/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express= require("express");
-var router= express.Router();
-var controllerStudent =require("../controllers/student-controllers.js");
-var controllerAddress=require("../controllers/address-controller.js");
-router.route("/students").get(controllerStudent.studentsGetAll);
-router.route("/students/:studentId").get(controllerStudent.studentGetOne);
-router.route("/students/:studentId/adress").get(controllerAddress.addressGetAll);
-router.route("/students/:studentId/adress/:addressId").get(controllerAddress.addressGetOne);
-module.exports=router;
